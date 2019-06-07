79aa55a2e6432977314f934c6861c9f85e048bf1
Francisco Cagnoni <fcagnoni@Franciscos-MacBook-Pro.local>
2019-06-06 21:12:08 -0600

    this should work maybe?

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,16 +3,16 @@ const path = require('path');
 module.exports = function (app){
 
     app.get('/', function(req, res){
-        res.sendFile(path.join(__dirname, '/frontEnd/home.html'))
+        res.sendFile(path.join(__dirname, '/home'))
     });
 
     app.get('/tables', function(req, res) {
-        res.sendFile(path.join(__dirname, '/frontEnd/tables.html'));
+        res.sendFile(path.join(__dirname, '/tables'));
         console.log("view table page"); 
       });
 
     app.get('/reserve', function(req, res){
-        res.sendFile(path.join(__dirname, '/frontEnd/reserve.html'));
+        res.sendFile(path.join(__dirname, '/reserve'));
         console.log("make reservation page");  
     });
 
